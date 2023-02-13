0db08a56b64699941d761d6887ee129522041b12
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2023-02-13 14:19:07 +0100

    ioListWeezer

---
+++ i.o.list.ino
@@ -0,0 +1,45 @@
+/* I/O LIST
+ * 
+ * BUTTONS
+ * B1: 3
+ * 
+ * BLUETOOTH
+ * HR: 2
+ * HT: 4
+ * 
+ * GRIPPER
+ * GR: 13
+ * 
+ * ROTATION SENSORS
+ * R2: 8
+ * R1: 1
+ * 
+ * MOTOR B
+ * B2: 5
+ * B1: 7
+ * 
+ * MOTOR A
+ * A2: 12
+ * A1: 11
+ * 
+ * NEWPIXEL
+ * NI: 6
+ * 
+ * SENSOR
+ * Trig: 9
+ * Echo: 10
+ * 
+ * LINE SENSOR
+ * From 0 to 7 Analog
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
