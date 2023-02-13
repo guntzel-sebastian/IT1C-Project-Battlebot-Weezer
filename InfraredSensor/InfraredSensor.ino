e6204795924e6857d9cd8b4c1f6e5a2aa1dce813
guntzel-sebastian <sebastian.guntzel@student.nhlstenden.com>
2023-02-13 14:52:58 +0100

    Simple IR sensor program using a library

---
+++ InfraredSensor/InfraredSensor.ino
@@ -0,0 +1,31 @@
+#include <QTRSensors.h>
+
+QTRSensors qtr;
+
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+
+void setup()
+{
+  // configure the sensors
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A7, A6, A5, A4, A3, A2, A1, A0}, SensorCount);
+  qtr.setEmitterPin(13);
+
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  qtr.read(sensorValues);
+
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  Serial.println();
+
+  delay(250);
+}
